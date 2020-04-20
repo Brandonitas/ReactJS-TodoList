8c1e4e8d7941331fef653cc94e97b0bc8d3d3b1b
Brandon Reyes <brandon.reyesmi@gmail.com>
2020-04-19 19:40:58 -0500

    Get request implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
+// app.js
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+import Create from './components/todos/Create';
+import Index from './components/todos/Index';
+
 function App() {
+
+  let todosList;
+
+  const [todos, setTodos] = useState([]);
+
+
+  useEffect(() => {
+      const fetchData = async() =>{
+      // You can await here
+      const response = await axios.get('http://localhost:3000/api/tasks')
+      .then(data =>{
+        setTodos(data.data)
+        console.log(data.data[0].description)
+      });
+      // ...
+    }
+    fetchData();
+  }, []); // Or [] if effect doesn't need props or state
+
+  const addTodo = (description, callback) => {
+    /*let cTodos = Object.assign([], todos);
+    cTodos.push({description: description, status: 'pending'});
+    setTodos(cTodos);*/
+    let body = {description: description};
+    axios.post('http://localhost:3000/api/tasks',body)
+      .then(data =>{
+        console.log(data);
+      });
+
+  }
+
+  const markAsDone = (task) => {
+    let cTodos = Object.assign([], todos);
+    cTodos[task].status = 'done';
+    setTodos(cTodos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Create addTodo={addTodo}/>
+      <Index todos={todos} markAsDone={markAsDone}/>
     </div>
   );
 }
---
+++ src/components/todos/Index.js
@@ -0,0 +1,35 @@
+// src/components/todos/Index.js
+import React, { useState } from 'react';
+
+const Index = ({todos, markAsDone}) => {
+
+    const handleMarkAsDone = (event, index) => {
+        markAsDone(index);
+    }
+    return (
+    <table border="1">
+      <thead>
+        <th>#</th>
+        <th>Task</th>
+        <th></th>
+      </thead>
+      <tbody>
+        {todos.map((todo, i) => {
+          return (
+            <tr key={i} style={{backgroundColor: todo['status'] == 'pending' ? 'green' : 'grey'}}>
+              <td>#{i}</td>
+              <td>{todo['description']}</td>
+              <td>
+                {todo['status'] == 'pending' && (
+                  <input type="button" value="terminado?" onClick={(event) => handleMarkAsDone(event, i)}/>
+                )}
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default Index;
